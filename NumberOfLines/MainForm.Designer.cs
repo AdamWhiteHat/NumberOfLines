1e788f3c115e8a0abfba32e4b3b5fe41a0582d64
AdamRakaska <arakaska@gmail.com>
2017-04-17 23:38:30 -0600

    Initial code check-in

---
+++ NumberOfLines/MainForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace NumberOfLines
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.tbFolderPath = new System.Windows.Forms.TextBox();
+			this.btnBrowse = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.labelNumberOfLines = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// tbFolderPath
+			// 
+			this.tbFolderPath.Location = new System.Drawing.Point(12, 32);
+			this.tbFolderPath.Name = "tbFolderPath";
+			this.tbFolderPath.ReadOnly = true;
+			this.tbFolderPath.Size = new System.Drawing.Size(340, 20);
+			this.tbFolderPath.TabIndex = 0;
+			// 
+			// btnBrowse
+			// 
+			this.btnBrowse.Location = new System.Drawing.Point(356, 28);
+			this.btnBrowse.Name = "btnBrowse";
+			this.btnBrowse.Size = new System.Drawing.Size(68, 28);
+			this.btnBrowse.TabIndex = 1;
+			this.btnBrowse.Text = "Browse...";
+			this.btnBrowse.UseVisualStyleBackColor = true;
+			this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(8, 16);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(63, 13);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Folder path:";
+			// 
+			// labelNumberOfLines
+			// 
+			this.labelNumberOfLines.AutoSize = true;
+			this.labelNumberOfLines.Location = new System.Drawing.Point(92, 60);
+			this.labelNumberOfLines.Name = "labelNumberOfLines";
+			this.labelNumberOfLines.Size = new System.Drawing.Size(94, 13);
+			this.labelNumberOfLines.TabIndex = 3;
+			this.labelNumberOfLines.Text = "                             ";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(36, 60);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(53, 13);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "# of lines:";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(428, 91);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.labelNumberOfLines);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.btnBrowse);
+			this.Controls.Add(this.tbFolderPath);
+			this.MaximumSize = new System.Drawing.Size(444, 129);
+			this.MinimumSize = new System.Drawing.Size(444, 129);
+			this.Name = "MainForm";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox tbFolderPath;
+		private System.Windows.Forms.Button btnBrowse;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label labelNumberOfLines;
+		private System.Windows.Forms.Label label2;
+	}
+}
+
---
+++ NumberOfLines/MainForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumberOfLines
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void btnBrowse_Click(object sender, EventArgs e)
+		{
+			string directoryPath = GetFolderDialog();
+			if (string.IsNullOrWhiteSpace(directoryPath))
+			{
+				return;
+			}
+
+			tbFolderPath.Text = directoryPath + "\\*";
+
+			DirectoryInfo dirInfo = new DirectoryInfo(directoryPath);
+			if (!dirInfo.Exists)
+			{
+				return;
+			}
+
+			IEnumerable<string> foundFiles = Directory.EnumerateFiles(dirInfo.FullName, "*.cs", SearchOption.AllDirectories);
+
+			int numberOfLines = foundFiles.Select(fn => GetNumberOfLines(fn)).Sum();
+
+			labelNumberOfLines.Text = numberOfLines.ToString();
+		}
+
+		private string GetFolderDialog()
+		{
+			FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+			if (folderDialog.ShowDialog() == DialogResult.OK)
+			{
+				return folderDialog.SelectedPath;
+			}
+			return string.Empty;
+		}
+
+		private int GetNumberOfLines(string filePath)
+		{
+			return File.ReadLines(filePath).Count();
+		}
+	}
+}
